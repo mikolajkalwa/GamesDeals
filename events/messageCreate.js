0e6e3f700f1a088b25c1d30450330dae47387dc2
Mikołaj Kalwa <mikolajkalwa@gmail.com>
2018-07-16 14:35:26 +0200

    ignores other bots, pretends infinite loops

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,7 +4,7 @@ const config = require('../config');
 
 module.exports = (bot => ({
     generator: (message) => {
-        if (message.content === `<@${config.botID}>`) {
+        if (!message.author.bot && message.content === `<@${config.botID}>`) {
             bot.createMessage(message.channel.id, `<@${message.author.id}> my prefix is \`${config.prefix}\` For more info use \`_help\``)
                 .catch(msgErr => logger.warn(`Cant send prefix ${msgErr}`));
         }
