048d6c0ff777d8719548f382206b7c271b57f64f
Mikołaj Kalwa <mikolajkalwa@gmail.com>
2018-08-08 13:38:25 +0200

    improvments

--- functions/broadcast.js
+++ functions/broadcast.js
@@ -7,25 +7,47 @@ const { Webhook } = require('../models/webhook');
 
 const { removeWebhook } = require('./removeWebhook');
 
+const milisecondsInTenMinutes = 600000;
+let failed = 0;
+let interval = 0;
+
+
+const gatewayErrorHandler = (content) => {
+    if (failed < 10) {
+        logger.info('Cant execute webhook. Trying again...');
+        if (interval < milisecondsInTenMinutes) {
+            interval += milisecondsInTenMinutes;
+        }
+        broadcast(content); // eslint-disable-line
+    } else {
+        logger.warn(`Tried ${failed} times, failed`);
+    }
+};
+
 const broadcast = (...message) => {
-    const content = _.unescape(message.join(' '));
-    Webhook.find()
-        .then((webhooks) => {
-            webhooks.forEach((webhook) => {
-                axios.post(`https://discordapp.com/api/webhooks/${webhook.id}/${webhook.token}`, {
-                    content,
-                })
-                    .catch((webhookExecuteErr) => {
-                        const { response: { status } } = webhookExecuteErr;
-                        if (status === 404 || status === 401) {
-                            removeWebhook(webhook.id);
-                        } else {
-                            logger.error(`Failed executing webhook. Guild: ${webhook.guild_id}. Error: ${webhookExecuteErr}`);
-                        }
-                    });
-            });
-        })
-        .catch(err => logger.error(`Failed fetching webhooks from database ${err}`));
+    setTimeout(() => {
+        const content = _.unescape(message.join(' '));
+        Webhook.find()
+            .then((webhooks) => {
+                webhooks.forEach((webhook) => {
+                    axios.post(`https://discordapp.com/api/webhooks/${webhook.id}/${webhook.token}`, {
+                        content,
+                    })
+                        .catch((webhookExecuteErr) => {
+                            const { response: { status } } = webhookExecuteErr;
+                            if (status === 404 || status === 401) {
+                                removeWebhook(webhook.id);
+                            } else if (status === 502) {
+                                failed += 1;
+                                gatewayErrorHandler(content);
+                            } else {
+                                logger.error(`Failed executing webhook. Guild: ${webhook.guild_id}. Error: ${webhookExecuteErr}`);
+                            }
+                        });
+                });
+            })
+            .catch(err => logger.error(`Failed fetching webhooks from database ${err}`));
+    }, interval);
 };
 
 module.exports = { broadcast };
