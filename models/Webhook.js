e2dfe9f3d8c390c16992258966ffa4cfe7942f3d
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2019-03-09 19:37:19 +0100

    Created Webhook model

---
+++ models/Webhook.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Webhook = mongoose.model('Webhook', {
+  id: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  guild_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = Webhook;
