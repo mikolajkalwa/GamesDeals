fc78a51e7cc85d588a47fb7626c53e094add7e2e
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2019-03-09 19:37:06 +0100

    Created Deal model

---
+++ models/Deal.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Deal = mongoose.model('Deal', {
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Deal;
