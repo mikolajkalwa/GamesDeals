a77203fc279b9a99f4658b8955c106bf6493736c
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2019-04-02 00:20:42 +0200

    more statistics about webhooks execution

--- routes/execute.js
+++ routes/execute.js
@@ -18,15 +18,26 @@ router.post('/execute', async (req, res) => {
     });
     const responses = await Promise.all(promiseArray.map(p => p.catch(e => e)));
     const webhooksToRemove = [];
+    let rateLimitedWebhooks = 0;
+    let failedWebhooks = 0;
     responses.forEach((response) => {
       const { statusCode } = response.request.res;
       if (statusCode === 404 || statusCode === 401) {
         const webhookID = response.config.url.split('/')[5];
         webhooksToRemove.push(webhookID);
       }
+      if (statusCode === 429) {
+        // TODO: implement some kind of throttling to prevent rate limiting.
+        rateLimitedWebhooks += 1;
+      }
+      if (statusCode >= 500 && statusCode < 600) {
+        // TODO: implement retry mechanism
+        failedWebhooks += 1;
+      }
     });
+    // deletes invalid / outdated webhooks from the database
     const removedWebhooks = await Webhook.deleteMany({ webhook_id: { $in: webhooksToRemove } });
-    res.status(200).send({ removedWebhooks });
+    res.status(200).send({ removedWebhooks, rateLimitedWebhooks, failedWebhooks });
   } catch (e) {
     res.status(500).send(e);
   }
