97718e906fdd33c2a56bf7a06e6717c556b8a8f3
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2019-04-08 12:33:28 +0200

    1st commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Games Deals Notifier
+
+
+## Description
+
+GamesDeals is Discord bot build with discord.js. GamesDeals informs about games which price was reduced to 0. It uses https://www.reddit.com/r/GameDeals as a source. This repository is responsible for searching and notifying the API about new games.  
+
+## Installation
+
+1. Configure [GamesDealsAPI](https://github.com/mikolajkalwa/GamesDealsAPI).
+2. Clone this repo,
+3. Run `npm install`.
+4. Configure the required environment variables.
+5. Configure your system scheduler to run [index.js](index.js) every 2 hours.
+
+### Required environment variables
+
+1. `API_URL` for example http://localhost:8080
+
+## Licence 
+
+This project is licensed under the GNU GPLv3 License - see the [LICENSE](LICENSE) file for details.
---
+++ index.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-await-in-loop */
+const axios = require('axios');
+const logger = require('pino')();
+const isFree = require('./lib/isFree.js');
+
+(async () => {
+  try {
+    const apiUrl = process.env.API_URL;
+    const response = await axios.get('https://www.reddit.com/r/GameDeals/hot/.json?limit=3');
+    if (response.status === 200) {
+      const threads = response.data.data.children;
+      const dealsToBroadcast = [];
+      for (const thread of threads) { // eslint-disable-line no-restricted-syntax
+        const { data: { id, url, title } } = thread;
+        if (isFree(title)) {
+          const getThreadFromDatabase = await axios.get(`${apiUrl}/deals/${id}`);
+          if (getThreadFromDatabase.status === 204) {
+            dealsToBroadcast.push({ title, url });
+            await axios.post(`${apiUrl}/deals/`, {
+              thread_id: id,
+              title,
+              url,
+            });
+          }
+        }
+      }
+      logger.info(dealsToBroadcast);
+      if (dealsToBroadcast.length > 0) {
+        let message = '';
+        dealsToBroadcast.forEach((deal) => {
+          message += `${deal.title} ${deal.url}\n`;
+        });
+        const executeWebhooks = await axios.post(`${apiUrl}/execute`, {
+          message,
+        });
+        if (executeWebhooks.status === 200) {
+          logger.info(`Completed! Removed webhooks: ${executeWebhooks.data.removedWebhooks.deletedCount}. Rate limited webhooks (didn't execute): ${executeWebhooks.data.rateLimitedWebhooks}. Webhooks which failed to execute: ${executeWebhooks.data.failedWebhooks}.`);
+        } else {
+          logger.warn(`Something went wrong during webhooks execution. Response status ${executeWebhooks.status}`);
+        }
+      }
+    }
+  } catch (e) {
+    logger.error(e);
+  }
+})();
