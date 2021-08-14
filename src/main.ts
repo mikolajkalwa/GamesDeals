d78eb82d64d999e6ac84918300591eab91e6dd7d
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-08-14 15:27:37 +0200

    fix: make pino logger injectable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
 import AppModule from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
