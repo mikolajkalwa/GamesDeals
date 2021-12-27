e694b43502ffbaf559cabe95d63706868b75d5d5
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-12-27 12:56:39 +0100

    feat: remove top.gg integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import './env';
 
-import got from 'got';
 import bot from './lib/bot';
 import loadEvents from './lib/modulesLoader';
-import Time from './helpers/Time';
 
 async function bootstrap() {
   loadEvents();
@@ -13,20 +11,6 @@ async function bootstrap() {
     name: 'Use gd:help',
     type: 3,
   });
-
-  if (process.env.BOT_ID && process.env.TOPGG_TOKEN) {
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    setInterval(async () => {
-      await got.post(`https://top.gg/api/bots/${process.env.BOT_ID as string}/stats`, {
-        json: {
-          server_count: bot.guilds.size,
-        },
-        headers: {
-          Authorization: process.env.TOPGG_TOKEN,
-        },
-      });
-    }, 30 * Time.MINUTE);
-  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
