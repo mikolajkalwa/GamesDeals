d32a6e44392f9f34f91783ec45d658a2034d83b3
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2022-02-05 20:16:53 +0100

    test: add e2e tests for create webhook

--- test/webhooks.e2e-spec.ts
+++ test/webhooks.e2e-spec.ts
@@ -99,4 +99,134 @@ describe('Deals', () => {
         .expect(204);
     });
   });
+
+  describe('POST', () => {
+    it('create basic webhook', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '1000',
+          token: 'super-secret',
+          guild: '17',
+          channel: '1',
+        })
+        .expect(201);
+
+      expect(response.body).toEqual(expect.objectContaining({
+        id: '1000',
+        token: 'super-secret',
+        guild: '17',
+        channel: '1',
+        mention: null,
+        keywords: [],
+        blacklist: [],
+      }));
+    });
+
+    it('create webhook with mention', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '1001',
+          token: 'super-secret',
+          guild: '17',
+          channel: '1',
+          mention: '@here',
+        })
+        .expect(201);
+
+      expect(response.body).toEqual(expect.objectContaining({
+        id: '1001',
+        token: 'super-secret',
+        guild: '17',
+        channel: '1',
+        mention: '@here',
+        keywords: [],
+        blacklist: [],
+      }));
+    });
+
+    it('create webhook with mention, keywords and blacklist', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '1002',
+          token: 'super-secret',
+          guild: '17',
+          channel: '1',
+          mention: '@here',
+          keywords: ['steam', 'epic'],
+          blacklist: ['indiegala', 'itch.io'],
+        })
+        .expect(201);
+
+      expect(response.body).toEqual(expect.objectContaining({
+        id: '1002',
+        token: 'super-secret',
+        guild: '17',
+        channel: '1',
+        mention: '@here',
+        keywords: ['steam', 'epic'],
+        blacklist: ['indiegala', 'itch.io'],
+      }));
+    });
+
+    it('create webhook with id alredy existing in the db', async () => {
+      await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '1002',
+          token: 'super-secret',
+          guild: '17',
+          channel: '1',
+          mention: '@here',
+          keywords: ['steam', 'epic'],
+          blacklist: ['indiegala', 'itch.io'],
+        })
+        .expect(409);
+    });
+
+    it('create webhook with requiered fields as empty strings', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '',
+          token: '',
+          guild: '',
+          channel: '',
+        })
+        .expect(400);
+
+      expect(response.body.message).toEqual(expect.arrayContaining([
+        'id should not be empty',
+        'id must be a number string',
+        'token should not be empty',
+        'guild should not be empty',
+        'guild must be a number string',
+        'channel should not be empty',
+        'channel must be a number string',
+      ]));
+    });
+
+    it('create webhook with completly invalid keywords and blacklist', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/webhooks')
+        .send({
+          id: '1111',
+          token: 'super-secret',
+          guild: '12',
+          channel: '1',
+          keywords: ['', 'abc', 'abc', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus est, vestibulum at magna non, semper consectetur sapien. Suspendisse id finibus sapien. Integer ut lorem ac diam pharetra maximus non vel felis. Donec scelerisque rutrum rutrum. Aliquam eget neque in nibh condimentum condimentum. Praesent maximus laoreet lectus, non ultricies arcu blandit a. Nam tristique placerat felis, vitae mollis dui elementum eget. Cras convallis odio sapien, at rhoncus ipsum bibendum sit amet. Nunc vel ultricies leo. Nulla facilisi. Curabitur sed ante a leo efficitur posuere sit amet sed erat. Vestibulum vehicula porttitor quam, vitae convallis arcu dictum eu. Aliquam erat volutpat.'],
+          blacklist: ['', 'cba', 'cba', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a tortor in enim scelerisque laoreet. Aliquam eget ornare sapien. Nunc eu congue urna. Donec facilisis ipsum quis quam tempor, sit amet imperdiet urna pretium. Donec euismod purus sed metus pretium, at ultricies augue efficitur. Donec pretium vel velit eu ullamcorper. Morbi non varius ipsum. Nunc in nisl volutpat, bibendum tellus sit amet, tincidunt diam. Ut tincidunt commodo ligula quis convallis. Sed sed pharetra massa. Praesent posuere dui at metus congue, tempus varius lectus elementum. Mauris lacinia id tortor quis ullamcorper. Maecenas blandit et turpis non ullamcorper. Praesent sit amet turpis.'],
+        })
+        .expect(400);
+
+      expect(response.body.message).toEqual(expect.arrayContaining([
+        "All keywords's elements must be unique",
+        'each value in keywords must be longer than or equal to 3 and shorter than or equal to 300 characters',
+        "All blacklist's elements must be unique",
+        'each value in blacklist must be longer than or equal to 3 and shorter than or equal to 300 characters',
+      ]));
+    });
+  });
 });
