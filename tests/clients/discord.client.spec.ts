3a3cd6892b1aaae8eefe9710221daf6abfaf9ea6
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2022-11-10 17:27:53 +0100

    test: skip discord client tests if webhook is not configured

--- tests/clients/discord.client.spec.ts
+++ tests/clients/discord.client.spec.ts
@@ -3,8 +3,13 @@ import { DiscordClient } from '../../src/clients';
 const discordClient = new DiscordClient('https://discord.com');
 
 describe('executeWebhook', () => {
+  if (!process.env['TEST_WEBHOOK_ID'] || !process.env['TEST_WEBHOOK_TOKEN']) {
+    test.only('Required test configuration is missing!!!', () => {
+      console.error('Required test configuration is missing!!!')
+      expect(true).toBe(true)
+    })
+  }
   it('should be possible to execute webhook', async () => {
-
     const webhookExecute = discordClient.executeWebhook({
       blacklist: [],
       keywords: [],
