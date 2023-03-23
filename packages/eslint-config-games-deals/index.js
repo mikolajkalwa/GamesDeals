59c8cedc11897b954b639e79f1961aec5c1997c4
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2023-03-23 20:21:54 +0100

    refactor: move eslint rules to its own package

---
+++ packages/eslint-config-games-deals/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "extends": [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  "rules": {
+    "no-console": 0,
+    "linebreak-style": 0,
+    "max-len": 0,
+    "no-return-await": 0,
+    "@typescript-eslint/return-await": [
+      "error",
+      "always"
+    ]
+  }
+}
